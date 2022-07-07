c246bc80b502f4afeff7f2501f45eee3e67ab5c7
Deril Raju <47169600+deril2605@users.noreply.github.com>
2022-07-07 22:21:36 +0530

    clf fix

--- svm_classifier.py
+++ svm_classifier.py
@@ -47,7 +47,7 @@ y = data.target
 y[y==0]=-1
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-svm_clf = MYSVM()
+clf = MYSVM()
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
